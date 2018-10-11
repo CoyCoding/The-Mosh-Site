bd28fb88a9d9c462f60e6f910df7053a8dcf32cc
fluffytoycoy <40921499+fluffytoycoy@users.noreply.github.com>
2018-10-11 00:48:53 -0500

    Add files via upload

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int? Id)
+        {       
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+        
+        private IEnumerable<Customer> GetCustomers()
+        {
+            //temp data until database connection implemented
+            return new List<Customer>
+            {
+               new Customer{Id = 1, Name = "Joe Somebody"},
+               new Customer{Id = 2, Name = "Jane Somebody"},
+               new Customer{Id = 3, Name = "Albert Pipes"},
+            };
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+
+        public ActionResult Random()
+        {
+            const string movieName = "Shrek!";
+            var movie = new Movie() { Id = 1, Name = movieName };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var movieCustomerViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(movieCustomerViewModel);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+               new Movie{Id = 1, Name = "Shrek"},
+               new Movie{Id = 2, Name = "Shrek 2"},
+               new Movie{Id = 3, Name = "Shrek 3"},
+            };
+        }
+    }
+}
